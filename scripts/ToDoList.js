db0e473a8a3b66b3aa1b955b692c84d1e5d489b0
Nikita Zhidunov <zhidunov@gmail.com>
2020-05-23 15:28:40 +0300

    Improved to-do list, fixed bug with switching to the main page

--- scripts/ToDoList.js
+++ scripts/ToDoList.js
@@ -3,9 +3,22 @@ let btn = document.querySelector(".block__form-btn");
 let blockList = document.querySelector(".block__list");
 
 const createNewElement = (text) => {
-    let newElem = document.createElement("p");
+    let newElem = document.createElement("li");
+    let deleteBtn = document.createElement("button");
+
+    deleteBtn.className = "block__list-delete";
+    deleteBtn.textContent = "Сделано";
+
     newElem.className = "block__list-elem";
     newElem.textContent = text;
+    newElem.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', ()=>{
+        newElem.className = "block__list__elem-delete";
+        setTimeout(()=>{
+            blockList.removeChild(newElem);
+        }, 1000);
+        
+    })
     blockList.appendChild(newElem);
 }
 
